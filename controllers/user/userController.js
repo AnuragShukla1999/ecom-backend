6e1949f05da4e9627596c863807495baca933e76
Anurag Shukla <shuklaanurag310@gmail.com>
2024-07-15 18:15:47 +0530

    .

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,5 +1,6 @@
 import userModal from "../../models/userModel";
 import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken';
 
 
 export const signup = async (req, res) => {
@@ -29,3 +30,56 @@ export const signup = async (req, res) => {
         console.error("Error", error);
     }
 }
+
+
+
+
+export const signin = async (req, res) => {
+
+    const { email, password } = req.body;
+    try {
+        const validUser = await userModal.findOne({ email });
+        
+        if (!validUser) {
+            return res.status(401).json({
+                message: "user not exist"
+            })
+        };
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if (!validPassword) {
+            return res.status(401).json({
+                message: "Wrong Password"
+            })
+        };
+
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.cookie('access_token', token).json({
+            message: "Sign in Successfully",
+            token,
+            validUser,
+            success: true
+        })
+    } catch (error) {
+        console.error(error)
+    }
+};
+
+
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('access_token');
+
+        res.status(201).json({
+            message: "Logout Successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Error in Logout Api"
+        })
+    }
+};
