78933283f3716b84b699e2482995b72b5b9e41ab
Anurag Shukla <shuklaanurag310@gmail.com>
2024-07-31 16:20:29 +0530

    .

--- controllers/cart/addToCartViewProduct.js
+++ controllers/cart/addToCartViewProduct.js
@@ -1,3 +1,21 @@
+import { dbConnection } from "../../db/dbConnection.js";
 
 
 
+export const addToCartViewProduct = async (req, res) => {
+    const { userId } = req.query;
+
+    if (!userId) {
+        return res.status(400).json({ message: 'userId is required' });
+    }
+
+    try {
+        const [cartItems] = await dbConnection.promise().query(
+            'SELECT c.*, p.name, p.description, p.price FROM cart c JOIN products p ON c.product_id = p.id WHERE c.userId = ?',
+            [userId]
+        );
+        res.json(cartItems);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
